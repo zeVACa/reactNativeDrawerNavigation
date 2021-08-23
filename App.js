8b25450fe29a4bf76e22d48842abfb059c861ed4
Nikita <logunik@bk.ru>
2021-08-23 12:02:31 +0300

    drawer example is working

---
+++ App.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View, Button} from 'react-native';
+
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Feed" component={Feed} />
+      <Drawer.Screen name="Article" component={Article} />
+    </Drawer.Navigator>
+  );
+}
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      {/* <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      /> */}
+      <Text>HomeScreen</Text>
+    </View>
+  );
+}
+
+function NotificationsScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      {/* <Button onPress={() => navigation.goBack()} title="Go back home" /> */}
+      <Text>NotificationsScreen</Text>
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Drawer.Navigator initialRouteName="Home">
+//         <Drawer.Screen name="Home" component={HomeScreen} />
+//         <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+//       </Drawer.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
